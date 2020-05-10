75f0b5bfe5cc9901228652946ce04e898d073e5e
Adenson <15594522896@163.com>
2020-05-10 08:22:30 +0800

    项目源码

---
+++ MemoryPool/PageCache.h
@@ -0,0 +1,156 @@
+#pragma once
+#include"Public.h"
+//单例模式
+class PageCache
+{
+public:
+	Span* NewSpan(size_t numpage); 
+	Span* _NewSpan(size_t numpage);
+	Span* GetIdToSpan(PAGE_ID id);
+	void ReleaseSpanToPageCache(Span* span);
+
+	static PageCache& GetInstance()
+	{
+		static PageCache Inst;
+		return Inst;
+	}
+	
+private:
+	SpanList _spanLists[MAX_PAGES];
+	unordered_map<PAGE_ID, Span*> _idSpanMap;
+	mutex _mtx;
+
+	PageCache()
+	{}
+	PageCache(const PageCache& PageCache) = delete;
+};
+
+Span* PageCache::_NewSpan(size_t numpage)
+{
+	if (!_spanLists[numpage].Empty())
+	{
+		Span* span = _spanLists[numpage].Begin();
+		_spanLists[numpage].PopFront();
+		return span;
+	}
+	for (size_t i = numpage + 1; i < MAX_PAGES; ++i)
+	{
+		if (!_spanLists[i].Empty())
+		{
+			Span* span = _spanLists[i].Begin();
+			_spanLists[i].PopFront();
+			//将Span的后半部分做为newspan进行返回是为了减少映射关系的次数。
+			Span* newspan = new Span;//newspan为分裂后的span的后半部分
+			newspan->_pageid = span->_pageid + span->_pagesize - numpage;
+			newspan->_pagesize = numpage;
+			for (PAGE_ID i = 0; i < numpage; i++)
+			{
+				_idSpanMap[newspan->_pageid + i] = newspan;
+			}
+			span->_pagesize -= numpage;
+			_spanLists[span->_pagesize].PushFront(span);
+			return newspan;
+		}
+	}
+	void* ptr = SystemAlloc(MAX_PAGES - 1);//向系统申请128页内存
+	Span* bigspan = new Span;
+
+	bigspan->_pageid = (PAGE_ID)ptr >> PAGE_SHIFT;
+	bigspan->_pagesize = MAX_PAGES - 1;
+
+	//将申请出来内存的每一页的_pageid都映射一个Span*,刚开始的多个页号都映射同一个Span*
+	for (PAGE_ID i = 0; i < bigspan->_pagesize; i++)
+	{
+		_idSpanMap[bigspan->_pageid + i] = bigspan;
+	}
+	_spanLists[bigspan->_pagesize].PushFront(bigspan);
+	return _NewSpan(numpage);//此次巧妙设计调用递归,多思考
+}
+
+//为了防止递归锁
+Span* PageCache::NewSpan(size_t numpage)
+{
+	_mtx.lock();
+	Span* span = _NewSpan(numpage);
+	_mtx.unlock();
+
+	return span;
+}
+Span* PageCache::GetIdToSpan(PAGE_ID id)
+{
+	auto it = _idSpanMap.find(id);
+	if (it != _idSpanMap.end())
+	{
+		return it->second;
+	}
+	else
+	{
+		return nullptr;
+	}
+}
+
+void PageCache::ReleaseSpanToPageCache(Span* span)
+{
+	//向前合并页
+	while (1)
+	{
+		PAGE_ID prevPageID = span->_pageid - 1;
+		auto prevIt = _idSpanMap.find(prevPageID);
+		//前面的页PageCache未申请
+		if (prevIt == _idSpanMap.end())
+		{
+			break;
+		}
+		//如果_usecount != 0,说明前面的页还在使用，不能合并
+		Span* prevSpan = prevIt->second;
+		if (prevSpan->_usecount != 0)
+		{
+			break; 
+		}
+		//合并
+		//如果合并超过128页的span，那么就不要合并了
+		if (span->_pagesize + prevSpan->_pagesize >= MAX_PAGES)
+		{
+			break;
+		}
+		span->_pageid = prevSpan->_pageid;
+		span->_pagesize += prevSpan->_pagesize;
+		for (PAGE_ID i = 0; i < prevSpan->_pagesize; ++i)
+		{
+			_idSpanMap[prevSpan->_pageid + i] = span;
+		}
+		_spanLists[prevSpan->_pagesize].Erase(prevSpan);
+		delete prevSpan;
+	}
+
+	//向后合并页
+	while (1)
+	{
+		PAGE_ID nextPageID = span->_pageid + span->_pagesize;
+		auto nextIt = _idSpanMap.find(nextPageID);
+		//后面的页PageCache未申请
+		if (nextIt == _idSpanMap.end())
+		{
+			break;
+		}
+		//如果_usecount != 0,说明后面的页还在使用，不能合并
+		Span* nextSpan = nextIt->second;
+		if (nextSpan->_usecount != 0)
+		{
+			break;
+		}
+		//如果合并超过128页的span，那么就不要合并了
+		if (span->_pagesize + nextSpan->_pagesize >= MAX_PAGES)
+		{
+			break;
+		}
+		span->_pagesize += nextSpan->_pagesize;
+		for (PAGE_ID i = 0; i < nextSpan->_pagesize; ++i)
+		{
+			_idSpanMap[nextSpan->_pageid + i] = span;
+		}
+		_spanLists[nextSpan->_pagesize].Erase(nextSpan);
+		delete nextSpan;
+	}
+		_spanLists[span->_pagesize].PushFront(span);
+}
---
+++ MemoryPool/ThreadCache.h
@@ -0,0 +1,113 @@
+#pragma once
+#include"Public.h"
+#include"CentralCache.h"
+
+class ThreadCache
+{
+public:
+	// 申请内存
+	void* Allocte(size_t size);
+	//释放内存
+	void Deallocte(void* ptr, size_t size);
+	// 从中心缓存获取对象
+	void* GetSpace_FromCentralCache(size_t index);
+	//如果自由链表中的对象超过一定长度就需要释放到CentralCache
+	void ListTooLong(Freelist& freelist, size_t num,size_t size);
+private:
+	Freelist _freeLists[NFREE_LIST];
+};
+
+//线程TLS
+//一个线程内部的各个函数调用都能访问、但其它线程不能访问的变量（被称为static memory local to a thread 线程局部静态变量),也就是TLS
+_declspec (thread) static ThreadCache* tlsThreadCache = nullptr;
+
+//申请内存
+void* ThreadCache::Allocte(size_t size)
+{
+	size_t index = SizeClass::ListIndex(size);
+	Freelist& freeList = _freeLists[index];
+	if (!freeList.Empty())
+	{
+		return freeList.Pop();
+	}
+	else
+	{
+		return GetSpace_FromCentralCache(SizeClass::RoundUp(size));
+	}
+}
+
+
+//释放内存
+void ThreadCache::Deallocte(void* ptr, size_t size)
+{
+	size_t index = SizeClass::ListIndex(size);
+	Freelist& freelist = _freeLists[index];
+	freelist.Push(ptr);
+
+	size_t num = SizeClass::NumMoveSize(size);
+	if (freelist.Num() >= num)
+	{
+		ListTooLong(freelist, num, size);
+	}
+}
+
+void ThreadCache::ListTooLong(Freelist& freelist, size_t num, size_t size)
+{
+	void* start = nullptr;
+	void* end = nullptr;
+	freelist.popRange(start, end, num); 
+	Next_Obj(end) = nullptr;
+	CentralCache::GetInstance().ReleaseListToSpans(start, size);
+}
+
+//当 _freeLists[index] 为 nullptr 时，从 CentralCache 中取空间
+void* ThreadCache::GetSpace_FromCentralCache(size_t size)
+{
+	size_t num = SizeClass::NumMoveSize(size);	
+	void* start = nullptr;
+	void* end = nullptr;
+	size_t actualNum = CentralCache::GetInstance().GetRangeObj(start, end, num, size);
+	if (actualNum == 1)//如果只获取到就一个直接返回
+	{
+		return start;
+	}
+	else
+	{
+		//如果获取到多个，就返回一个，将剩下的挂起来
+		size_t index = SizeClass::ListIndex(size);
+		Freelist& list = _freeLists[index];
+		list.pushRange(Next_Obj(start), end, actualNum - 1);
+		return start;
+	} 
+}
+
+
+
+
+
+
+
+
+
+//独立测试ThreadCache
+////当 _freeLists[index] 为 nullptr 时，从 CentralCache 中取空间
+//void* ThreadCache::GetSpace_FromCentralCache(size_t index)
+//{
+//	size_t num = 10;
+//	size_t size = (index + 1) * 8;//通过 index 计算 size,然后再开 num 个 size 大小的空间
+//	char* start = (char*)malloc(size * num);
+//	char* cur = start;
+//	for (size_t i = 0; i < num -1; i++)
+//	{
+//		char* next = cur + size;//此处通过 cur + size 决定了每一段空间
+//		Next_Obj(cur) = next;
+//		cur = next;
+//	}
+//	Next_Obj(cur) = nullptr;
+//
+//	void* head = Next_Obj(start);
+//	void* tail = cur;
+//
+//	_freeLists[index].pushRange(head, tail);
+//	return start;
+//}
