ca5217e2872090cac8becbc69edffaf60c6bfcce
Adenson <15594522896@163.com>
2020-05-15 21:32:37 +0800

    项目源码

--- P2P Downloader/main.cpp
+++ P2P Downloader/main.cpp
@@ -1,9 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#include"util.h"
+#include"util.hpp"
+#include"httplib.h"
+void helloworld(const httplib::Request& req, httplib::Response& rsp)
+{
+
+	rsp.set_content("<html><h1>helloworld</h1></htm>", "text/html");
+	rsp.status = 200;
+}
 int main()
 {
-	std::vector<Adapter> list;
-	AdapterUtil::GetAllAdapter(&list);
+	httplib::Server srv;
+	srv.Get("/", helloworld);
+	srv.listen("0.0.0.0", 9000);
 	system("pause");
 	return 0;
 }
